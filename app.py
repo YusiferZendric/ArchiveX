943a3d0b2e2e2988f43a1685a81ed36be4e977f9
YusiferZendric <yusiferzendric@gmail.com>
2022-03-21 22:21:49 +0530

    Added login system

--- app.py
+++ app.py
@@ -11,29 +11,65 @@ class save(db.Model):
     level = db.Column(db.Integer,nullable=False)
     weakness = db.Column(db.String(400),nullable=False)
     subject = db.Column(db.String(20),nullable=False)
-
-    def __init__(self, level, weakness,subject):
+    email = db.Column(db.String(40),nullable=False)
+    password = db.Column(db.String(10), nullable=False)
+    def __init__(self, level, weakness,subject,email,password):
         self.level = level
         self.weakness = weakness
         self.subject = subject
-        
-@app.route("/")
-def main():
+        self.email = email
+        self.password = password
+
+@app.route("/intro")
+def intro():
     return render_template("intro.html")
+@app.route("/login!", methods=['GET','POST'])
+def login_save():
+    if request.method=="POST":
+        email = request.form['email']
+        password = request.form['password']
+        user = save.query.filter_by(email=email,password=password).first()
+        if user:
+            session['email'] = email
+            session['password'] = password
+
+            return render_template("intro.html")
+        else:
+            return render_template("login.html",message="Invalid email or password or Register first!")
+@app.route("/")
+def login():
+    return render_template("login.html")
+@app.route("/register")
+def register():
+    return render_template("register.html")
+@app.route("/register-try", methods=['GET','POST'])
+def register_try():
+    if request.method=='POST':
+        newEmail = request.form['newEmail']
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+        if password1!=password2:
+            return render_template("register.html",msg="Those Passwords didn't match")
+        else:
+            session['user'] = newEmail
+            session['password'] = password1
+        return render_template("success.html",success=success)
 @app.route("/home")
 def home():
     return render_template('index.html')
 @app.route("/view")
 def view():
-
-    return render_template('view.html',)
+    return render_template('view.html')
 
 @app.route("/<subject>")
 def submitMistakes(subject):
     if subject!="favicon.ico":
         session['subject'] = subject
     if subject=="favicon.ico":
-        subject = session['subject']
+        try:
+            subject = session['subject']
+        except:
+            pass
     return render_template('subject.html', subject=subject)
 @app.route("/success", methods=['GET',"POST"])
 def success():
@@ -45,11 +81,15 @@ def success():
         data = save.query.filter_by(level=option, weakness=weakness).first()
         print(data)
         if data is None:
-            data = save(option, weakness,session['subject'])
-            db.session.add(data)
-            db.session.commit()
-            print(f"subject: {session['subject']}")
-            return render_template('success.html',success="success")
+            try:
+                data = save(option, weakness,session['subject'],session['email'],session['password'])
+                db.session.add(data)
+                db.session.commit()
+                print(f"subject: {session['subject']}")
+                return render_template('success.html',success="success")
+            except:
+                return render_template('success.html',create_account="fail")
+
         else:
             return render_template('success.html',warning="warning",subject=session['subject'])
 
@@ -70,7 +110,12 @@ def subject_view(sub,level):
             Data[rows.subject]['level'] = [rows.level]
             Data[rows.subject]['weakness'] = [rows.weakness]         
     print(Data)
-    return render_template('view.html',data=Data,subject=sub,list=list, len=len,level=level)
+    try:
+        a = len(Data[sub]['level'])
+        return render_template('view.html',data=Data,subject=sub,list=list, len=len,level=level)
+    except Exception as e:
+        print(e)
+        return render_template('success.html', nothing="nothing")
 
 if __name__ == '__main__':
     app.run(debug=True)
